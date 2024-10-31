70dfb324e5fc54a5c42d0b5119fd211b63b956e2
Max <marm73583@gmail.com>
2024-10-31 22:08:28 +0300

    t

---
+++ src/Individ2/Chocolate.java
@@ -0,0 +1,13 @@
+package Individ2;
+
+public class Chocolate {
+    String country_exporter_bobs;
+    String company_exporter;
+    String brand;
+    int index_expertisa;
+    int year_expertisa;
+    int percent_contain;
+    String country_brand;
+    int grade;
+
+}
--- src/Individ2/Main.java
+++ src/Individ2/Main.java
@@ -1,6 +1,15 @@
-package Individ2; //
+package Individ2; // 53
+
+import lab07.SchoolBoy;
+
+import java.util.ArrayList;
+import java.util.TreeMap;
 
 public class Main {
+    public static void main(String[] args) {
+        TreeMap<Byte, ArrayList<SchoolBoy>> info = new TreeMap<>();
+        
 
+    }
 
 }
